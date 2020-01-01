810804fd57d1f307724ac56731b69641c8f1c7cc
somnaths14 <somnaths14@gmail.com>
2020-01-01 23:08:15 +0530

    First Commit to GIT

---
+++ src/Test/firstTstNG.java
@@ -0,0 +1,11 @@
+package Test;
+import org.testng.annotations.Test;
+
+
+public class firstTstNG {
+
+	@Test
+	public void printTest() {
+		System.out.print("Hello");
+	}
+}
---
+++ src/Test/secondTest.java
@@ -0,0 +1,9 @@
+package Test;
+import org.testng.annotations.Test;
+
+public class secondTest {
+	@Test
+	public void print2() {
+		System.out.println("hi");
+	}
+}
